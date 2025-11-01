37b1f1b5c9ffef13374a3e52114044679c853ace
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-11-01 11:53:50 +0100

    c

--- python/psa_checksum.py
+++ python/psa_checksum.py
@@ -22,6 +22,6 @@ def psa_checksum(address: int, sig, d: bytearray) -> int:
              0x2F6: 0x8  # 758 decimal - messagmessage ACC2
              }.get(address, 0xB)
   byte = sig.start_bit // 8
-  d[byte] &= 0x0F if sig.start % 8 >= 4 else 0xF0
+  d[byte] &= 0x0F if sig.start_bit % 8 >= 4 else 0xF0
   checksum = sum((b >> 4) + (b & 0xF) for b in d)
   return (chk_ini - checksum) & 0xF
